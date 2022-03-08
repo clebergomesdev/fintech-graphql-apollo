c0ae85e5f40317d824631d41c23edbf008a0d4bf
Cleber Gomes <cleber.gomes@govoll.com>
2022-03-08 11:41:59 -0300

    Initial commit

---
+++ src/app.module.ts
@@ -0,0 +1,62 @@
+import { Module } from '@nestjs/common';
+import { GATEWAY_BUILD_SERVICE, GraphQLGatewayModule } from '@nestjs/graphql';
+import { RemoteGraphQLDataSource } from '@apollo/gateway';
+// import { decode } from 'jsonwebtoken';
+
+class AuthenticatedDataSource extends RemoteGraphQLDataSource {
+  async willSendRequest({ request, context }) {
+    // const { userId } = await decode(context.jwt);
+    // request.http.headers.set('x-user-id', userId);
+  }
+}
+
+@Module({
+  providers: [
+    {
+      provide: AuthenticatedDataSource,
+      useValue: AuthenticatedDataSource,
+    },
+    {
+      provide: GATEWAY_BUILD_SERVICE,
+      useFactory: (AuthenticatedDataSource) => {
+        return ({ name, url }) => new AuthenticatedDataSource({ url });
+      },
+      inject: [AuthenticatedDataSource],
+    },
+  ],
+  exports: [GATEWAY_BUILD_SERVICE],
+})
+class BuildServiceModule {}
+
+@Module({
+  imports: [
+    GraphQLGatewayModule.forRootAsync({
+      useFactory: async () => ({
+        gateway: {
+          serviceList: [
+            // { name: 'users', url: 'http://user-service/graphql' },
+            // { name: 'posts', url: 'http://post-service/graphql' },
+            {
+              name: 'financial',
+              url: 'https://staging-financial-api.herokuapp.com/graphql',
+            },
+          ],
+        },
+        server: {
+          playground: true,
+          introspection: true,
+          cors: {
+            origin: '*',
+            credentials: true,
+          },
+          context: ({ req }) => ({
+            jwt: req.headers.authorization,
+          }),
+        },
+      }),
+      imports: [BuildServiceModule],
+      inject: [GATEWAY_BUILD_SERVICE],
+    }),
+  ],
+})
+export class AppModule {}
